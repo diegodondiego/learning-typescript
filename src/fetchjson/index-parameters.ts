01caf6664608706e69bce3ac9e2c535d2493668a
Diego Martins <dondiego7@gmail.com>
2023-11-28 19:58:12 -0800

    info for parameters

--- src/fetchjson/index-parameters.ts
+++ src/fetchjson/index-parameters.ts
@@ -4,6 +4,8 @@ let id = "1";
 
 if (process.argv[2] && process.argv[2] == "--id") {
   id = process.argv[3];
+} else {
+  console.log("no specific id informed. to add an specific id use --id");
 }
 
 const url = 'https://jsonplaceholder.typicode.com/todos/' + id;
