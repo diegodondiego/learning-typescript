c41546bd6d8856336997664bf4903b85e46b4d81
Diego Martins <dondiego7@gmail.com>
2023-11-28 19:55:13 -0800

    back to basic config

--- src/fetchjson/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-var url = 'https://jsonplaceholder.typicode.com/todos';
-axios_1.default.get(url).then(function (response) {
-    console.log(response.data);
-});
