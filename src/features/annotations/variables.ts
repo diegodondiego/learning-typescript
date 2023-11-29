23e01d45c402cc27cf0523dda5edf268dd5ce0d2
Diego Martins <dondiego7@gmail.com>
2023-11-28 21:08:32 -0800

    feat: types and annotations

---
+++ src/features/annotations/variables.ts
@@ -0,0 +1,8 @@
+let apples: number = 5;
+
+apples = 10.1;
+
+let spped: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
---
+++ src/features/types.ts
@@ -0,0 +1,12 @@
+const today = new Date();
+today.getMonth();
+
+const person = {
+    age: 41
+}
+
+class Color {
+    rgb: number
+}
+
+const red = new Color();
