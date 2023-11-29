fd533758d6db49bb44e4fd8a0ff58b8839710a95
Diego Martins <dondiego7@gmail.com>
2023-11-29 13:01:16 -0800

    wip: learning variables reference

--- src/features/annotations/variables.ts
+++ src/features/annotations/variables.ts
@@ -6,3 +6,36 @@ let spped: string = 'fast';
 let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colours: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true]
+
+// Classes
+class Car {
+
+}
+let car: Car
+
+// Object literal
+let point: {x: number; y: number} = {
+    x: 10,
+    y: 20
+}
+
+// Function
+// hard to read
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+}
+
+// when to use annotations
+// 1) fx that returns the 'any' type
+const json = '{ "x": 10, "y": 20 }'
+const coordinates = JSON.parse(json);
+console.log(coordinates); // { x: 10, y: 20 }
+
